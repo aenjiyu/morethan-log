fdd01d2c2d3da84321c9714368be99e81ee6906d
aenjiyu <154419295+aenjiyu@users.noreply.github.com>
2023-12-24 17:18:08 +0900

    Update ContactCard.tsx

--- src/routes/Feed/ContactCard.tsx
+++ src/routes/Feed/ContactCard.tsx
@@ -11,7 +11,7 @@ import styled from "@emotion/styled"
 const ContactCard: React.FC = () => {
   return (
     <>
-      <StyledTitle>💬 Contact</StyledTitle>
+      <StyledTitle>🔗 SNS</StyledTitle>
       <StyledWrapper>
          {CONFIG.profile.blog && (
           <a
